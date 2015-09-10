53d6b37e4440831bf645779489e5cacefb24c985
bekbulatov <alexander@bekbulatov.ru>
2015-09-10 18:16:01 +0300

    Fix dependence from v8monoctx

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ import os
 from setuptools import setup, Extension
 
 V8_PREFIX = os.environ.get('V8_PREFIX')
-V8_VERSION = os.environ.get('V8_VERSION')
 
-if not V8_PREFIX or not V8_VERSION:
-    print 'You should set V8_PREFIX and V8_VERSION in the environment'
+if not V8_PREFIX:
+    print 'You should set V8_PREFIX variable in the environment'
     exit(1)
 
 V8_LIBRARY_DIR = os.path.join(V8_PREFIX, 'lib64/')
@@ -15,10 +14,10 @@ V8_INCLUDE_DIR = os.path.join(V8_PREFIX, 'include/')
 setup(
     name='PyV8Mono',
     description='Python binding for V8 MonoContext library',
-    version='3.00',
+    version='1.0',
     author='Bekbulatov Alexander',
     author_email='alexander@bekbulatov.ru',
-    url='https://gitlab.corp.mail.ru/portal/v8monocontext',  # FIXME
+    url='https://github.com/bekbulatov/PyV8Mono',
     packages=['PyV8Mono'],
     ext_modules=[
         Extension('PyV8Mono.monocontext',
@@ -29,7 +28,7 @@ setup(
                 V8_LIBRARY_DIR,
             ],
             libraries=[
-		'v8monoctx.%s' % V8_VERSION,
+                'v8monoctx',
             ],
             sources=[
                 'monocontext.cpp',
